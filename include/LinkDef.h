7ab56e32c864386bde1e9696f47f454fa90a8193
majidkazemi89 <36406173+majidkazemi89@users.noreply.github.com>
2021-08-12 23:45:38 +0200

    link Definition

---
+++ include/LinkDef.h
@@ -0,0 +1,15 @@
+ #ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ struct PhysicVar+;
+#pragma link C++ struct PhysicVec+;
+
+#pragma link C++ class std::vector<PhysicVar>+;
+#pragma link C++ class std::vector<PhysicVec>+;
+#pragma link C++ class std::vector<PhysicVar*>+;
+#pragma link C++ class std::vector<PhysicVec*>+;
+
+#endif
